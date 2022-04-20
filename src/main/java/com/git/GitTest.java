bccfd9072143adc618033c5f3999dd3798da2488
zyq <294056189@qq.com>
2022-04-20 08:45:11 +0800

    first commit

---
+++ src/main/java/com/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.git;
+
+/**
+ * @author zyq
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
